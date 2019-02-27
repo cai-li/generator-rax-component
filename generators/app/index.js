2a70f560e85e21afebe9d960fefed441e563dc1b
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-27 12:05:38 +0800

    feat: add rax component generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,6 +10,15 @@ const fs = require('fs');
 
 
 module.exports = class extends Generator {
+  constructor(args, options) {
+    super(args, options);
+    this.option('rax', {
+      alias: 'r',
+      value: 'rax',
+      type: String,
+      desc: 'create rax component contains each index and css file'
+    })
+  }
   initializing() {
     try {
       this.username = process.env.USER || process.env.USERPROFILE.split(require('path').sep)[2]
@@ -19,16 +28,15 @@ module.exports = class extends Generator {
   }
 
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the rad ${chalk.red('generator-typescript-jest-sdk')} generator!`)
-    );
+    if (this.options.rax) {
+      this.log(
+        yosay(`Welcome to the ${chalk.red('rax-component')} generator!`)
+      );
 
-    const prompts = [
-      {
+      const prompts = [{
         type: 'input',
         name: 'name',
-        message: 'Your project name',
+        message: 'Set your component name',
         validate: name => {
           if (!name) {
             return 'Project name cannot be empty'
@@ -37,52 +45,88 @@ module.exports = class extends Generator {
             return 'Project name should only consist of 0~9, a~z, A~Z, _, .'
           }
 
-          const fs = require('fs')
           if (!fs.existsSync(this.destinationPath(name))) {
             return true;
           }
+
           if (fs.statSync(this.destinationPath(name)).isDirectory()) {
             return 'Project already exist'
           }
+
           return true
         }
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Your project description',
-        default: ''
-      },
-      {
-        type: 'input',
-        name: 'keywords',
-        message: 'Your project keywords',
-        default: ''
-      },
-      {
-        type: 'input',
-        name: 'username',
-        message: 'Your name',
-        default: this.username
-      },
-      {
-        type: 'list',
-        name: 'registry',
-        message: 'Which registry would you use?',
-        choices: [
-          'https://registry.npm.taobao.org',
-          'https://registry.npmjs.org'
-        ]
-      }
-    ];
+      }];
 
-    return this.prompt(prompts).then(answers => {
-      // To access props later use this.props.someAnswer;
-      const keywords = answers.keywords;
-      this.answers = answers;
-      this.obj = {answers: this.answers};
+      return this.prompt(prompts).then(answers => {
+        this.answers = answers;
+        this.obj = { answers: this.answers };
+      });
+    } else {
+      this.log(
+        yosay(`Welcome to the ${chalk.red('rax-page')} generator!`)
+      );
 
-    });
+      const prompts = [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Your project name',
+          validate: name => {
+            if (!name) {
+              return 'Project name cannot be empty'
+            }
+
+            if (!/\w+/.test(name)) {
+              return 'Project name should only consist of 0~9, a~z, A~Z, _, .'
+            }
+
+            if (!fs.existsSync(this.destinationPath(name))) {
+              return true;
+            }
+
+            if (fs.statSync(this.destinationPath(name)).isDirectory()) {
+              return 'Project already exist'
+            }
+            return true
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Your project description',
+          default: ''
+        },
+        {
+          type: 'input',
+          name: 'keywords',
+          message: 'Your project keywords',
+          default: ''
+        },
+        {
+          type: 'input',
+          name: 'username',
+          message: 'Your name',
+          default: this.username
+        },
+        {
+          type: 'list',
+          name: 'registry',
+          message: 'Which registry would you use?',
+          choices: [
+            'https://registry.npm.taobao.org',
+            'https://registry.npmjs.org'
+          ]
+        }
+      ];
+
+      return this.prompt(prompts).then(answers => {
+        // To access props later use this.props.someAnswer;
+        const keywords = answers.keywords;
+        this.answers = answers;
+        this.obj = { answers: this.answers };
+      });
+    }
+
   }
 
   configuring(answers) {
@@ -100,32 +144,43 @@ module.exports = class extends Generator {
 
 
   writing() {
-    const _ = require('lodash')
+    if (this.options.rax) {
+      this.fs.copyTpl(this.templatePath('page/index.js'), this.destinationPath('index.js'), this.obj)
 
-    this.fs.copy(this.templatePath('public', '*'), this.destinationPath('public'))
-    this.fs.copy(this.templatePath('src', '*'), this.destinationPath('src'));
-    this.fs.copy(this.templatePath('__tests__', '*'), this.destinationPath('__tests__'));
+    } else {
+      this.fs.copy(this.templatePath('app/public', '*'), this.destinationPath('public'))
+      this.fs.copy(this.templatePath('app/src', '*'), this.destinationPath('src'));
+      this.fs.copy(this.templatePath('app/__tests__', '*'), this.destinationPath('__tests__'));
 
-    this.fs.copy(this.templatePath('babel.config.js'), this.destinationPath('babel.config.js'))
+      this.fs.copy(this.templatePath('app/babel.config.js'), this.destinationPath('babel.config.js'))
+      this.fs.copy(this.templatePath('app/eslintignore'), this.destinationPath('.eslintignore'))
+      this.fs.copy(this.templatePath('app/gitignore'), this.destinationPath('.gitignore'))
+      this.fs.copy(this.templatePath('app/eslintrc'), this.destinationPath('.eslintrc'))
+      this.fs.copy(this.templatePath('app/LICENSE'), this.destinationPath('LICENSE'))
+      this.fs.copyTpl(this.templatePath('app/package.json_vm'), this.destinationPath('package.json'), this.obj)
 
-    this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'))
-    this.fs.copy(this.templatePath('eslintrc'), this.destinationPath('.eslintrc'))
-    this.fs.copy(this.templatePath('LICENSE'), this.destinationPath('LICENSE'))
-    this.fs.copyTpl(this.templatePath('package.json_vm'), this.destinationPath('package.json'), this.obj)
-
-
-    this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('README.md'), this.obj)
-
+      this.fs.copyTpl(this.templatePath('app/README.md'), this.destinationPath('README.md'), this.obj)
+    }
   }
 
   install() {
-    this.npmInstall(undefined, {
-      registry: this.answers.registry
-    })
+    if (this.options.rax) {
+
+    } else {
+      this.npmInstall(undefined, {
+        registry: this.answers.registry
+      })
+    }
+
   }
 
   end() {
-    this.log.ok('Project ' + this.answers.name + ' generated!!!')
-    this.spawnCommand('npm', ['run', 'test'])
+    if (this.options.rax) {
+      this.log.ok('Rax Component ' + this.answers.name + ' generated!!!')
+    } else {
+      this.log.ok('Project ' + this.answers.name + ' generated!!!')
+      this.spawnCommand('npm', ['run', 'test'])
+    }
+
   }
 };
